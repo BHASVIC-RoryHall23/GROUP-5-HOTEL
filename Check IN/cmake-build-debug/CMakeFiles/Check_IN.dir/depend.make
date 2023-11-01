177c853c11037c8fb7303596db7d6861c6eca027
Rory <rory.hall23@students.bhasvic.ac.uk>
2023-11-01 11:53:20 +0000

    Testing use of CSV in C

---
+++ Check IN/cmake-build-debug/CMakeFiles/Check_IN.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Check_IN.
+# This may be replaced when dependencies are built.
--- Check IN/main.c
+++ Check IN/main.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 
 
     FILE *fpt; // File Pointer
+    fpt = fopen("C:\\Users\\roryh\\Desktop\\MAIN\\CS\\C code GH\\GROUP-5-HOTEL\\Check IN\\Data.csv","r");
+    int test_num; char test_str; // TEST PURPOSE
 
-    int NumGuests, BoardType, StayLength, DailyNP;
+
+    int NumGuests, BoardType, StayLength, DailyNP, cost, room;
     char Name[30], BookingID[33];
 
+    /*
+    // Getting info from client.
+    printf("Please input your name:"); scanf("%s",*Name); printf("\n");
+    printf("Please input Number of Guests staying:"); scanf("%d",&NumGuests); printf("\n");
+    printf("Please input your BoardType (FB, HB, B&B) (1,2,3):"); scanf("%d",&BoardType); printf("\n");
+    printf("Please input if you want with Daily NewsPaper, 0 for no, 1 for yes:"); scanf("%d",&DailyNP); printf("\n");
+    */
+
+    // Checking if any rooms are open.
+    fscanf(fpt,"%d",&test_num);
+    printf("%d",test_num);
+
+
+
+    // Closing file
+    fclose(fpt);
 
 
 
