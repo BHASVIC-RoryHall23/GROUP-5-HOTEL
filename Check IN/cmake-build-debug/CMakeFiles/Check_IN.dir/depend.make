97abed38845e35f0854542c37a4b68e6b0fdc6fe
BHASVIC-RoryHall <rory.hall23@students.bhasvic.ac.uk>
2023-11-03 11:34:52 +0000

    Code compelete
    Code done need testing.

--- Check IN/cmake-build-debug/CMakeFiles/Check_IN.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Check_IN.
-# This may be replaced when dependencies are built.
--- Check IN/main.c
+++ Check IN/main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
+#include <stdlib.h>
 
 int main() {
 
 
     FILE *fpt; // File Pointer
-    fpt = fopen("C:\\Users\\roryh\\Desktop\\MAIN\\CS\\C code GH\\GROUP-5-HOTEL\\Check IN\\Data.csv","r");
-    int test_num; char test_str; // TEST PURPOSE
+    srand(time(NULL)); // Random number
 
+    int NumGuests, BoardType, StayLength, DailyNP, cost, room, RandomNumber,flag;
+    char Name[30], BookingID[33],FILENAME[9]="Room0.txt", SecondName[20];
 
-    int NumGuests, BoardType, StayLength, DailyNP, cost, room;
-    char Name[30], BookingID[33];
 
     /*
     // Getting info from client.
@@ -20,19 +21,125 @@ int main() {
     printf("Please input if you want with Daily NewsPaper, 0 for no, 1 for yes:"); scanf("%d",&DailyNP); printf("\n");
     */
 
-    // Checking if any rooms are open.
-    fscanf(fpt,"%d",&test_num);
-    printf("%d",test_num);
+    // Checking if rooms are free.
+
+    room = -1; // -1 means that no rooms are free.#
+    for(int x=0;x<6;x++){
+        FILENAME[4]=x;
+
+        fpt = fopen(FILENAME,"r"); // Opening each file.
+
+        if(fpt==NULL){
+
+            room = x+1; // Changing to show that a room is free, making it x to show which room is free.
+
+            switch(x+1){ // Printing out the rooms open and their cost.
+                case 1:
+                    printf("Room 1 is open and costs £100./n");
+                    break;
+
+                case 2:
+                    printf("Room 2 is open and costs £100./n");
+                    break;
+
+                case 3:
+                    printf("Room 3 is open and costs £85./n");
+                    break;
+
+                case 4:
+                    printf("Room 4 is open and costs £75./n");
+                    break;
+
+                case 5:
+                    printf("Room 5 is open and costs £75./n");
+                    break;
+
+                case 6:
+                    printf("Room 6 is open and costs £50./n");
+                    break;
+
+            }
+        }
+
+
+        fclose(fpt);// Closing file.
+    }
+    // I.e. room =-1 for closed and room = the room number if open.
+    if (room == -1){
+        printf("Sorry No rooms available!");
+    }
+
+    // Picking a room.
+
+    printf("Which room do you want?");
+    scanf("%d",room);
+
+
+    // Making Booking ID.
+    // Using in the format of FIRSTNAME SECONDMENT
+
+    for(int x=0;x<strlen(Name);x++){ // Finding the gap in between the first and second names i.e. Barron Green
+        if(Name[x] == ' ') {
+            flag=x; // Saving where the gap is.
+            break; // Moving out of the for loop.
+        }
+    }
+    for(int x=flag;x<strlen(Name);x++){
+        SecondName[x] = Name[x]; // Coping the second name into the variable.
+    }
+
+    // Creating random number.
+    RandomNumber = rand()%10; // Random number between 1-10;
+
+    //strcpy(BookingID, strcat(SecondName,itoa(RandomNumber)));
+    sprintf(BookingID, "%s%d",SecondName, RandomNumber);
 
 
 
-    // Closing file
-    fclose(fpt);
+    // Storing data.
+    /*
+     * NAME
+     * NUM GUESTS
+     * BOARD TYPE
+     * STAY LENGTH
+     * DAILY NS
+     * BOOKING ID
+     */
+    switch(room){
+        case 1:
+            fpt = fopen("Room1.txt","w+");
+            fprintf(fpt,"%s,%d,%d,%d,%s",Name,NumGuests,BoardType,StayLength,DailyNP,BookingID);
+            fclose(fpt);
+            break;
+        case 2:
+            fpt = fopen("Room2.txt","w+");
+            fprintf(fpt,"%s,%d,%d,%d,%s",Name,NumGuests,BoardType,StayLength,DailyNP,BookingID);
+            fclose(fpt);
+            break;
+        case 3:
+            fpt = fopen("Room3.txt","w+");
+            fprintf(fpt,"%s,%d,%d,%d,%s",Name,NumGuests,BoardType,StayLength,DailyNP,BookingID);
+            fclose(fpt);
+            break;
+        case 4:
+            fpt = fopen("Room4.txt","w+");
+            fprintf(fpt,"%s,%d,%d,%d,%s",Name,NumGuests,BoardType,StayLength,DailyNP,BookingID);
+            fclose(fpt);
+            break;
+        case 5:
+            fpt = fopen("Room5.txt","w+");
+            fprintf(fpt,"%s,%d,%d,%d,%s",Name,NumGuests,BoardType,StayLength,DailyNP,BookingID);
+            fclose(fpt);
+            break;
+        case 6:
+            fpt = fopen("Room6.txt","w+");
+            fprintf(fpt,"%s,%d,%d,%d,%s",Name,NumGuests,BoardType,StayLength,DailyNP,BookingID);
+            fclose(fpt);
+            break;
+
+    }
 
 
 
-
-
-
-        return 0;
+    return 0;
 }
