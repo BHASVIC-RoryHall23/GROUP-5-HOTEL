e58f6c7e78097cc284d0e88bedc1adef5db276a1
BHASVIC-RoryHall <rory.hall23@students.bhasvic.ac.uk>
2023-11-13 13:58:04 +0000

    Finishing
    Just chaning the txt file added to make it look nicer

--- Check IN/main.c
+++ Check IN/main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 #include <time.h>
 #include <stdlib.h>
 
@@ -11,16 +10,17 @@ int main() {
     FILE *fpt; // File Pointer
     srand(time(NULL)); // Random number
 
-    int NumGuests, BoardType, StayLength, DailyNP, room, RandomNumber,flag;
-    char Name[30], BookingID[33],FILENAME[9]="Room0.txt", SecondName[20];
+    int NumGuests, BoardType, StayLength, DailyNP, room, RandomNumber;
+    char Name[15], sName[15], BookingID[33],FILENAME[9]="Room0.txt";
 
 
 
     // Getting info from client.
-    printf("Please input your name:"); scanf("%s",*Name); printf("\n");
-    printf("Please input Number of Guests staying:"); scanf("%d",&NumGuests); printf("\n");
-    printf("Please input your BoardType (FB, HB, B&B) (1,2,3):"); scanf("%d",&BoardType); printf("\n");
-    printf("Please input if you want with Daily NewsPaper, 0 for no, 1 for yes:"); scanf("%d",&DailyNP); printf("\n");
+    printf("Please input your first name:"); scanf("%s",Name);
+    printf("Please input your second name:"); scanf("%s",sName);
+    printf("Please input Number of Guests staying:"); scanf("%d",&NumGuests);
+    printf("Please input your BoardType (FB, HB, B&B) (1,2,3):"); scanf("%d",&BoardType);
+    printf("Please input if you want with Daily NewsPaper, 0 for no, 1 for yes:"); scanf("%d",&DailyNP);
 
 
     // Checking if rooms are free.
@@ -77,27 +77,16 @@ int main() {
     // Picking a room.
 
     printf("Which room do you want?");
-    scanf("%d",room);
+    scanf("%d",&room);
 
 
     // Making Booking ID.
-    // Using in the format of FIRSTNAME SECONDMENT
-
-    for(int x=0;x<strlen(Name);x++){ // Finding the gap in between the first and second names i.e. Barron Green
-        if(Name[x] == ' ') {
-            flag=x; // Saving where the gap is.
-            break; // Moving out of the for loop.
-        }
-    }
-    for(int x=flag;x<strlen(Name);x++){
-        SecondName[x] = Name[x]; // Coping the second name into the variable.
-    }
 
     // Creating random number.
     RandomNumber = rand()%10; // Random number between 1-10;
 
     //strcpy(BookingID, strcat(SecondName,itoa(RandomNumber)));
-    sprintf(BookingID, "%s%d",SecondName, RandomNumber);
+    sprintf(BookingID, "%s%d",sName, RandomNumber);
 
 
 
@@ -113,32 +102,44 @@ int main() {
     switch(room){
         case 1:
             fpt = fopen("Room1.txt","w+");
-            fprintf(fpt,"%s,%d,%d,%d,%s",Name,NumGuests,BoardType,StayLength,DailyNP,BookingID);
+            fprintf(fpt,"Name: %s/n",Name);
+            fprintf(fpt,"No. Guests: %d/n",NumGuests);
+            fprintf(fpt,"Name: %s/n",Name);
+            fprintf(fpt,"Name: %s/n",Name);
+            fprintf(fpt,"Name: %s/n",Name);
+            fprintf(fpt,"Name: %s/n",Name);
+
+            printf("Added to book!");
             fclose(fpt);
             break;
         case 2:
             fpt = fopen("Room2.txt","w+");
-            fprintf(fpt,"%s,%d,%d,%d,%s",Name,NumGuests,BoardType,StayLength,DailyNP,BookingID);
+            fprintf(fpt,"%s,%d,%d,%d,%d,%s",Name,NumGuests,BoardType,StayLength,DailyNP,BookingID);
+            printf("Added to book!");
             fclose(fpt);
             break;
         case 3:
             fpt = fopen("Room3.txt","w+");
-            fprintf(fpt,"%s,%d,%d,%d,%s",Name,NumGuests,BoardType,StayLength,DailyNP,BookingID);
+            fprintf(fpt,"%s,%d,%d,%d,%d,%s",Name,NumGuests,BoardType,StayLength,DailyNP,BookingID);
+            printf("Added to book!");
             fclose(fpt);
             break;
         case 4:
             fpt = fopen("Room4.txt","w+");
-            fprintf(fpt,"%s,%d,%d,%d,%s",Name,NumGuests,BoardType,StayLength,DailyNP,BookingID);
+            fprintf(fpt,"%s,%d,%d,%d,%d,%s",Name,NumGuests,BoardType,StayLength,DailyNP,BookingID);
+            printf("Added to book!");
             fclose(fpt);
             break;
         case 5:
             fpt = fopen("Room5.txt","w+");
-            fprintf(fpt,"%s,%d,%d,%d,%s",Name,NumGuests,BoardType,StayLength,DailyNP,BookingID);
+            fprintf(fpt,"%s,%d,%d,%d,%d,%s",Name,NumGuests,BoardType,StayLength,DailyNP,BookingID);
+            printf("Added to book!");
             fclose(fpt);
             break;
         case 6:
             fpt = fopen("Room6.txt","w+");
-            fprintf(fpt,"%s,%d,%d,%d,%s",Name,NumGuests,BoardType,StayLength,DailyNP,BookingID);
+            fprintf(fpt,"%s,%d,%d,%d,%d,%s",Name,NumGuests,BoardType,StayLength,DailyNP,BookingID);
+            printf("Added to book!");
             fclose(fpt);
             break;
 
@@ -156,4 +157,16 @@ int is_empty_file(FILE *fp) {
     }
     ungetc(c, fp);
     return 0;
+}
+
+int boarding(int board){
+    char x[3];
+    if(board == 1){
+        x[0] = 'F';
+        X[1] = 'F';
+    }
+    elif(board == 2){
+        x[]
+    }
+
 }
