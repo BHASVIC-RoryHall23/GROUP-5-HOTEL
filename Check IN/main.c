3f6a788868f5b403939e58ee0b8f36d0e20e1ce6
BHASVIC-RoryHall <rory.hall23@students.bhasvic.ac.uk>
2023-11-15 08:50:54 +0000

    Validating Finished
    All validation for data inputs in done.

--- Check IN/main.c
+++ Check IN/main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 int is_empty_file(FILE *fp);
+int boarding(int board);
 
 int main() {
 
@@ -19,9 +20,24 @@ int main() {
     printf("Please input your first name:"); scanf("%s",Name);
     printf("Please input your second name:"); scanf("%s",sName);
     printf("Please input Number of Guests staying:"); scanf("%d",&NumGuests);
+
+    // Validating NumGuests.
+    while (NumGuests >4 || NumGuests < 1){
+        printf("Four or below please try again:"); scanf("%d",&NumGuests);
+    }
+
     printf("Please input your BoardType (FB, HB, B&B) (1,2,3):"); scanf("%d",&BoardType);
+
+    // Validating BoardType
+    while( BoardType != 1 && BoardType != 2 && BoardType != 3 ){
+        printf("Please input 1,2,3 For Fullbooking, halfBooking and Bread and breakfast:");scanf("%d",&BoardType);
+    }
+
     printf("Please input if you want with Daily NewsPaper, 0 for no, 1 for yes:"); scanf("%d",&DailyNP);
 
+    while( DailyNP != 1 && DailyNP != 0 ){
+        printf("Please input 0 or 1 not anything else. Try again:");scanf("%d",&DailyNP);
+    }
 
     // Checking if rooms are free.
 
@@ -102,49 +118,89 @@ int main() {
     switch(room){
         case 1:
             fpt = fopen("Room1.txt","w+");
-            fprintf(fpt,"Name: %s/n",Name);
-            fprintf(fpt,"No. Guests: %d/n",NumGuests);
-            fprintf(fpt,"Name: %s/n",Name);
-            fprintf(fpt,"Name: %s/n",Name);
-            fprintf(fpt,"Name: %s/n",Name);
-            fprintf(fpt,"Name: %s/n",Name);
+            fprintf(fpt,"Name: %s\n",Name);
+            fprintf(fpt,"No. Guests: %d\n",NumGuests);
+            fprintf(fpt,"BoardType: %d\n",BoardType);
+            fprintf(fpt,"StayLength: %d\n",StayLength);
+            fprintf(fpt,"Daily Newspaper: %d\n",DailyNP);
+            fprintf(fpt,"BookingID: %s\n",BookingID);
 
             printf("Added to book!");
             fclose(fpt);
             break;
+
         case 2:
             fpt = fopen("Room2.txt","w+");
-            fprintf(fpt,"%s,%d,%d,%d,%d,%s",Name,NumGuests,BoardType,StayLength,DailyNP,BookingID);
+
+            fprintf(fpt,"Name: %s\n",Name);
+            fprintf(fpt,"No. Guests: %d\n",NumGuests);
+            fprintf(fpt,"BoardType: %d\n",BoardType);
+            fprintf(fpt,"StayLength: %d\n",StayLength);
+            fprintf(fpt,"Daily Newspaper: %d\n",DailyNP);
+            fprintf(fpt,"BookingID: %s\n",BookingID);
+
             printf("Added to book!");
             fclose(fpt);
             break;
+
         case 3:
             fpt = fopen("Room3.txt","w+");
-            fprintf(fpt,"%s,%d,%d,%d,%d,%s",Name,NumGuests,BoardType,StayLength,DailyNP,BookingID);
+
+            fprintf(fpt,"Name: %s\n",Name);
+            fprintf(fpt,"No. Guests: %d\n",NumGuests);
+            fprintf(fpt,"BoardType: %d\n",BoardType);
+            fprintf(fpt,"StayLength: %d\n",StayLength);
+            fprintf(fpt,"Daily Newspaper: %d\n",DailyNP);
+            fprintf(fpt,"BookingID: %s\n",BookingID);
+
             printf("Added to book!");
             fclose(fpt);
             break;
+
         case 4:
             fpt = fopen("Room4.txt","w+");
-            fprintf(fpt,"%s,%d,%d,%d,%d,%s",Name,NumGuests,BoardType,StayLength,DailyNP,BookingID);
+
+            fprintf(fpt,"Name: %s\n",Name);
+            fprintf(fpt,"No. Guests: %d\n",NumGuests);
+            fprintf(fpt,"BoardType: %d\n",BoardType);
+            fprintf(fpt,"StayLength: %d\n",StayLength);
+            fprintf(fpt,"Daily Newspaper: %d\n",DailyNP);
+            fprintf(fpt,"BookingID: %s\n",BookingID);
+
             printf("Added to book!");
             fclose(fpt);
             break;
+
         case 5:
             fpt = fopen("Room5.txt","w+");
-            fprintf(fpt,"%s,%d,%d,%d,%d,%s",Name,NumGuests,BoardType,StayLength,DailyNP,BookingID);
+
+            fprintf(fpt,"Name: %s\n",Name);
+            fprintf(fpt,"No. Guests: %d\n",NumGuests);
+            fprintf(fpt,"BoardType: %d\n",BoardType);
+            fprintf(fpt,"StayLength: %d\n",StayLength);
+            fprintf(fpt,"Daily Newspaper: %d\n",DailyNP);
+            fprintf(fpt,"BookingID: %s\n",BookingID);
+
             printf("Added to book!");
             fclose(fpt);
             break;
+
         case 6:
             fpt = fopen("Room6.txt","w+");
-            fprintf(fpt,"%s,%d,%d,%d,%d,%s",Name,NumGuests,BoardType,StayLength,DailyNP,BookingID);
+
+            fprintf(fpt,"Name: %s\n",Name);
+            fprintf(fpt,"No. Guests: %d\n",NumGuests);
+            fprintf(fpt,"BoardType: %d\n",BoardType);
+            fprintf(fpt,"StayLength: %d\n",StayLength);
+            fprintf(fpt,"Daily Newspaper: %d\n",DailyNP);
+            fprintf(fpt,"BookingID: %s\n",BookingID);
+
             printf("Added to book!");
             fclose(fpt);
             break;
 
     }
-    printf("Done writting!");
+   // printf("Done writing!");
 
 
     return 0;
@@ -163,10 +219,21 @@ int boarding(int board){
     char x[3];
     if(board == 1){
         x[0] = 'F';
-        X[1] = 'F';
+        x[1] = 'B';
+        return *x;
     }
-    elif(board == 2){
-        x[]
+    else if(board == 2){
+        x[0] = 'H';
+        x[1] = 'B';
+        return *x;
     }
-
+    else if(board == 3){
+        x[0] = 'B';
+        x[1] = 'B';
+        return *x;
+    }
+    else{
+        printf("ERROR - B_TYPE");
+        return 0;
+    }
 }
