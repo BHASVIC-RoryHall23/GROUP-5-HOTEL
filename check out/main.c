f20003cf5ab4801ce074a17aa1ef5e0ea1075343
BHASVIC-KaiSmith23 <kai.smith23@students.bhasvic.ac.uk>
2023-11-17 11:15:46 +0000

    calculations tested + working

--- check out/main.c
+++ check out/main.c
@@ -7,8 +7,8 @@ int BoardPrice (int BoardRateB,int NumGuestsB, int NumOfChildrenB, int StayLengt
 int main() {
     FILE *filepointer;
 
-    int DayRateA, BoardRateA, room, StayLengthA, Age, NumOfChildrenA, NumGuestsA;
-    char Data[100], Name[20], NumGuest[20], BoardType[20], StayLength[20], DailyNP[20], ID[20];
+    int DayRateA, BoardRateA = 20, room, StayLengthA = 4, Age = 66, NumOfChildrenA = 2, NumGuestsA = 4, DailyNp = 1;
+    char Data[100], Name[20], NumGuest[20], BoardType[20]="HB", StayLength[20], DailyNP[20], ID[20];
     printf("Please enter your room number: ");
     fflush(stdin);
     scanf("%d", &room);
@@ -17,35 +17,65 @@ int main() {
     {
         case 1:
             DayRateA = 100;
-            filepointer = fopen("Room1.txt", "r");
+            //filepointer = fopen("Room1.txt", "r");
             break;
         case 2:
             DayRateA = 100;
-            filepointer = fopen("Room2.txt", "r");
+            //filepointer = fopen("Room2.txt", "r");
             break;
         case 3:
             DayRateA = 85;
-            filepointer = fopen("Room3.txt", "r");
+            //filepointer = fopen("Room3.txt", "r");
             break;
         case 4:
             DayRateA = 75;
-            filepointer = fopen("Room4.txt", "r");
+            //filepointer = fopen("Room4.txt", "r");
             break;
         case 5:
             DayRateA = 75;
-            filepointer = fopen("Room5.txt", "r");
+            //filepointer = fopen("Room5.txt", "r");
             break;
         case 6:
             DayRateA = 50;
-            filepointer = fopen("Room6.txt", "r");
+            //filepointer = fopen("Room6.txt", "r");
             break;
         default:
             printf("ERROR");
             break;
     }
+    /*/
+    switch (BoardType)
+    {
+        case BoardType == "FB":
+            BoardRateA = 20;
+            break;
+        case BoardType == "HB":
+            BoardRateA = 15;
+            break;
+        case BoardType == "BB":
+            BoardRateA = 5;
+            break;
+        default:
+            printf("board type not recognised");
+            break;
+    }
+/*/
+    //fscanf("%s", &Data);
+    float roomcost = RoomPrice(DayRateA, StayLengthA, Age);
+    float boardcost = BoardPrice(BoardRateA, NumGuestsA, NumOfChildrenA, StayLengthA);
 
-    fscanf("%s", &Data);
-    
+    printf("\n%.2f", roomcost);
+    printf("\n%.2f", boardcost);
+
+    float total = roomcost + boardcost;
+    if (DailyNp == 1)
+    {
+        printf("\nTotal Bill: %.2f",total + 5.5);
+    }
+    else
+    {
+        printf("\nTotal Bill: %.2f", total);
+    }
 }
 
 int RoomPrice(int DayRateB, int StayLengthB, int AgeB)
@@ -54,7 +84,7 @@ int RoomPrice(int DayRateB, int StayLengthB, int AgeB)
     RoomTotal = DayRateB * StayLengthB;
     if (AgeB > 65)
     {
-        RoomTotal = RoomTotal * 0.65;
+        RoomTotal = RoomTotal * 0.9;
     }
     return RoomTotal;
 }
